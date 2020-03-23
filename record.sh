27d5f07705fe273e8a7f922b9b98003a23542d83
Leon Si <leonzalion@gmail.com>
2020-03-23 18:15:39 -0400

    removed shell scripts

--- record.sh
+++
@@ -1,8 +0,0 @@
-num=0
-uuid=$(uuidgen)
-while [ 1 ]; do
-  printf -v numstr "%08d" $num;
-  screencapture -D 2 -t jpg -x ~/Desktop/WBSScreenshots/$uuid-$numstr.jpg;
-  num=$((num + 1));
-  sleep 60;
-done
--- transform.sh
+++
@@ -1,5 +0,0 @@
-printf "UUID: "
-read uuid
-printf "Name: "
-read name
-ffmpeg -framerate 24 -i ~/Desktop/WBSScreenshots/$uuid-%08d.jpg $name.mp4
